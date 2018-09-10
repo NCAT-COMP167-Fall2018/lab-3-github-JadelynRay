12ac65e39e51c377af166831630f8259e2710ad5
Jadelyn Ray <jkray@aggies.ncat.edu>
2018-09-10 17:52:11 -0400

    new feature 2

--- PersonalTwitterFeed/src/personaltwitterfeed/PersonalTwitterFeed.java
+++ PersonalTwitterFeed/src/personaltwitterfeed/PersonalTwitterFeed.java
@@ -5,51 +5,64 @@
  */
 package personaltwitterfeed;
 
+import java.util.Date;
 import java.util.Scanner;
-
 /**
  *
- * @author CCannon
+ * @author Jadelyn Ray
  */
 public class PersonalTwitterFeed {
 
-    private static int MAX_NUMBER_TWEETS = 200;
+	public static int MAX_NUMBER_TWEETS = 200;
     
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        String[] tweets = new String[MAX_NUMBER_TWEETS];
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("Welcome to your personal Twitter!");
-        System.out.println("What's your name, tweeter?");
-        
-        String tweeterName = keyboard.nextLine();
-        
-        System.out.println("Nice to meet you " + tweeterName + "!");
-        System.out.println("Enter your tweets and I will add them to your timeline!");
-        
-        int numTweets = 0;
-        
-        while(numTweets < (MAX_NUMBER_TWEETS - 1)) {
-            tweets[numTweets] = keyboard.nextLine();
-            numTweets++;
-            
-            System.out.println(tweeterName + "'s Personal Twitter Feed:");
-            for(int i = 0; i < numTweets; i++) {
-                System.out.println("- " + tweets[i]);
-            }
-            
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            
-            if(numTweets < (MAX_NUMBER_TWEETS - 1))
-                System.out.println("Enter your next tweet:");
-        }
-        
-        System.out.println("Your twitter feed is full");
-    }
+	/**
+ 	* @param args the command line arguments
+ 	*/
+	public static void main(String[] args) {
+    	String[] tweets = new String[MAX_NUMBER_TWEETS];
+    	newTweet(tweets);
+	}
     
-}
+	public static String getTimeStamp(){
+    	Date now = new Date();
+    	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSZ");
+    	return dateFormat.format(now);
+	}
+   	 
+	/**
+ 	*
+ 	* @param a
+ 	*/
+	public static void newTweet(String[] a) {
+    	Scanner keyboard = new Scanner(System.in);
+    	System.out.println("Welcome to your personal Twitter!");
+    	System.out.println("What's your name, tweeter?");
+   	 
+    	String tweeterName = keyboard.nextLine();
+   	 
+    	System.out.println("Nice to meet you " + tweeterName + "!");
+    	System.out.println("Enter your tweets and I will add them to your timeline!");
+   	 
+    	int numTweets = 0;
+   	 
+    	while(numTweets < (MAX_NUMBER_TWEETS - 1)) {
+        	a[numTweets] = keyboard.nextLine() + getTimeStamp();
+        	numTweets++;
+       	 
+        	System.out.println(tweeterName + "'s Personal Twitter Feed:");
+        	for(int i = 0; i < numTweets; i++) {
+            	System.out.println("- " + a[i]);
+        	}
+       	 
+        	System.out.println();
+        	System.out.println();
+        	System.out.println();
+        	System.out.println();
+       	 
+        	if(numTweets < (MAX_NUMBER_TWEETS - 1))
+            	System.out.println("Enter your next tweet:");
+    	}
+   	 
+    	System.out.println("Your twitter feed is full");
+	}
+ }
